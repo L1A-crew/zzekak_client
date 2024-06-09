ee971c687bbde1db68e3b136e70fa6b0ddecf15c
신원규 <won7963kr@gmail.com>
2024-06-09 14:50:01 +0900

    di resolve 를 entry function 에서 수행

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
+import 'package:zzekak/di/app_di_config.dart';
 import 'package:zzekak/zzekak_app.dart';
 
 late final GetIt getIt;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await resolveDependency();
   KakaoSdk.init(
       javaScriptAppKey: "1be9d48b92741228526daa7cb8e14ced",
       nativeAppKey: "5264e2b13b4d8bf7d5c0f42f68e11eb0");
--- app/lib/screen/splash/vm_splash.dart
+++ app/lib/screen/splash/vm_splash.dart
@@ -3,12 +3,10 @@
  * Copyright (c) . All rights reserved.
  */
 
-import 'package:zzekak/di/app_di_config.dart';
 import 'package:zzekak/zzekak_app.dart';
 
 class SplashViewModel {
   Future<AppRoutes> initLogic() async {
-    await resolveDependency();
     await Future.delayed(const Duration(seconds: 1));
     return AppRoutes.HOME;
   }
